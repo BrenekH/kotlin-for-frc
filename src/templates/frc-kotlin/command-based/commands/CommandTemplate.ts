f9a44ba0bd11a392930d21102d6886c8a33c2fc3
Brenek Harrison <brenekharrison@gmail.com>
2018-11-07 21:42:26 -0700

    build.gradle template update and README/CHANGELOG modifications

--- src/templates/frc-kotlin/command-based/commands/CommandTemplate.ts
+++ src/templates/frc-kotlin/command-based/commands/CommandTemplate.ts
@@ -20,12 +20,12 @@ class #{NAME}: Command() {
 
   // Called just before this Command runs the first time
   override fun initialize () {
-    TODO("Not Implemented")
+    //TODO("Not Implemented")
   }
 
   // Called repeatedly when this Command is scheduled to run
   override fun execute () {
-    TODO("Not Implemented")
+    //TODO("Not Implemented")
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -35,13 +35,13 @@ class #{NAME}: Command() {
 
   // Make this return true when this Command no longer needs to run execute()
   override fun end () {
-    TODO("Not Implemented")
+    //TODO("Not Implemented")
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   override fun interrupted () {
-    TODO("Not Implemented")
+    //TODO("Not Implemented")
   }
 }`;
   }
