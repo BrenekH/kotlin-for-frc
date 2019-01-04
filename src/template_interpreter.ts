51ebc924fde3f0731e9c70ee0a2591b7ed26a19e
MetaHerobrine1 <brenekharrison@gmail.com>
2019-01-04 12:38:02 -0700

    Pre major changes

--- src/template_interpreter.ts
+++ src/template_interpreter.ts
@@ -1,5 +1,6 @@
 //All Robot types
 import { MainTemplate } from './templates/frc-kotlin/Main';
+import { BuildGradleTemplate } from './templates/frc-kotlin/BuildGradle';
 
 // Command based
 import { CommandRobotTemplate } from './templates/frc-kotlin/command-based/Robot';
@@ -8,7 +9,11 @@ import { RobotMapTemplate } from './templates/frc-kotlin/command-based/RobotMap'
 import { SubsystemTemplate } from './templates/frc-kotlin/command-based/subsystems/SubsystemTemplate';
 import { CommandGroupTemplate } from './templates/frc-kotlin/command-based/commands/CommandGroupTemplate';
 import { CommandTemplate } from './templates/frc-kotlin/command-based/commands/CommandTemplate';
-import { BuildGradleTemplate } from './templates/frc-kotlin/BuildGradle';
+import { TimedCommandTemplate } from './templates/frc-kotlin/command-based/commands/TimedCommand';
+import { InstantCommandTemplate } from './templates/frc-kotlin/command-based/commands/InstantCommandTemplate';
+import { EmptyClassTemplate } from './templates/frc-kotlin/command-based/EmptyClassTemplate';
+import { PIDSubsystemTemplate } from './templates/frc-kotlin/command-based/subsystems/PIDSubsystemTemplate';
+import { TriggerTemplate } from './templates/frc-kotlin/command-based/triggers/TriggerTemplate';
 
 // Iterative
 import { IterativeRobotTemplate } from './templates/frc-kotlin/iterative/Robot';
@@ -18,11 +23,17 @@ import { SampleRobotTemplate } from './templates/frc-kotlin/sample/Robot';
 
 // Timed
 import { TimedRobotTemplate } from './templates/frc-kotlin/timed/Robot';
+import { TimedRobotSkeletonTemplate } from './templates/frc-kotlin/timed-skeleton/Robot';
 
 export enum templateType {
     subsystem,
     command,
     command_group,
+    empty_class,
+    instant_command,
+    timed_command,
+    pid_subsystem,
+    trigger,
     robot,
     oi,
     robot_map,
@@ -33,7 +44,8 @@ export enum robotType {
     command,
     sample,
     timed,
-    iterative
+    iterative,
+    timed_skeleton
 }
 
 export function parseTemplate(className: string, packageName: string, templatetype: templateType) {
@@ -50,7 +62,7 @@ export function parseTemplate(className: string, packageName: string, templatety
     // Package generation
     re = /#{PACKAGE}/gi;
     transformedData = transformedData.replace(re, packageName);
-    
+
     return transformedData;
 }
 
@@ -70,6 +82,16 @@ export function getTemplateObjectFromTemplateType(targetTemplateType: templateTy
             return new CommandGroupTemplate();
         case templateType.build_gradle:
             return new BuildGradleTemplate();
+        case templateType.empty_class:
+            return new EmptyClassTemplate();
+        case templateType.pid_subsystem:
+            return new PIDSubsystemTemplate();
+        case templateType.instant_command:
+            return new InstantCommandTemplate();
+        case templateType.timed_command:
+            return new TimedCommandTemplate();
+        case templateType.trigger:
+            return new TriggerTemplate();
     }
 }
 
@@ -83,6 +105,8 @@ export function getTemplateObjectFromRobotType(targetRobotType: robotType) {
             return new TimedRobotTemplate();
         case robotType.command:
             return new CommandRobotTemplate();
+        case robotType.timed_skeleton:
+            return new TimedRobotSkeletonTemplate();
     }
 }
 
