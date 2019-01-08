21314864ceab1a1932314cdeed6643e63b9c2014
MetaHerobrine1 <brenekharrison@gmail.com>
2019-01-07 22:01:36 -0700

    Addressed Issues #5 and #6

--- src/templates/frc-kotlin/Main.ts
+++ src/templates/frc-kotlin/Main.ts
@@ -9,23 +9,29 @@ export class MainTemplate {
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
-package frc.robot
-
-import edu.wpi.first.wpilibj.RobotBase
-
+    
+package frc.robot;
+    
+import edu.wpi.first.wpilibj.RobotBase;
+    
 /**
  * Do NOT add any static variables to this class, or any initialization at all.
  * Unless you know what you are doing, do not modify this file except to
  * change the parameter class to the startRobot call.
  */
-class Main {
-    /**
-     * Main initialization function. Do not perform any initialization here.
-     * 
-     * <p>If you change your main robot class, change the parameter type.
-    */
-   fun main(args: Array<String>) = RobotBase.startRobot(::Robot)
+public final class Main {
+  private Main() {
+  }
+   
+  /**
+   * Main initialization function. Do not perform any initialization here.
+   *
+   * <p>If you change your main robot class, change the parameter type.
+   */
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+    
+  }
 }
  `;
   }
