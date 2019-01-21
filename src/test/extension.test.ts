215085864424087522dd2c97e7b62cb9d02fe979
Brenek Harrison <brenekharrison@gmail.com>
2019-01-20 23:26:38 -0700

    Travis Testing

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -16,7 +16,6 @@ suite("Extension Tests", function () {
 
     // Defines a Mocha unit test
     test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+        assert.equal(1, 1);
     });
 });
