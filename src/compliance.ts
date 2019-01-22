36fbdc87c2bcd9597399de890215ff546750c2bd
Brenek Harrison <brenekharrison@gmail.com>
2019-01-21 18:01:22 -0700

    On the way to completing compliance testing

--- src/compliance.ts
+++ src/compliance.ts
@@ -1,8 +1,20 @@
+import { targetGradleRioVersion } from "./constants";
+import * as preferences from "./preferences";
+
 export function isBuildGradleCompliant(): boolean {
+    console.log("Checking build.gradle compliance");
+    let registeredVersion = preferences.getWPILibVersion();
+    if (registeredVersion === targetGradleRioVersion) {
+        return true;
+    }
     return false;
 }
 
 export function isMainKtCompliant(): boolean {
+    console.log("Checking Main.kt compliance");
+    if (preferences.getMainKt()) {
+        return true;
+    }
     return false;
 }
 
