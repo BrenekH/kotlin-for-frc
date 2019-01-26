c136720eb2cd89aee4477d1478c5b70250eb8a23
Brenek Harrison <brenekharrison@gmail.com>
2019-01-25 20:01:27 -0700

    Testing tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -8,14 +8,24 @@ import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-// import * as vscode from 'vscode';
+import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
+// import * as path from "path";
+// import * as fs from 'fs';
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function () {
 
     // Defines a Mocha unit test
     test("Something 1", function() {
+        if (typeof vscode.workspace.workspaceFolders === 'undefined') {
+			console.log("Not a valid workspace");
+            assert.equal(1, 0);
+            return;
+        }
+        console.log(vscode.workspace.workspaceFolders[0].uri.fsPath);
+        console.log(vscode.workspace.workspaceFolders[0].uri.fsPath.replace("/out/test/", "/testing-workspace/workspace"));
+        // console.log(fs.readFileSync(vscode.workspace.workspaceFolders[0].uri.fsPath.replace("/out/test/", "/testing-workspace/workspace") + "/src/frc/robot/Main.kt"));
         assert.equal(1, 1);
     });
 });
