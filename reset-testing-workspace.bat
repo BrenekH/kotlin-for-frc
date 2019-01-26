6375036740a43425e18272edb3a2229bc41cc907
Brenek Harrison <brenekharrison@gmail.com>
2019-01-25 18:36:39 -0700

    Windows test workspace reset script complete

--- reset-testing-workspace.bat
+++ reset-testing-workspace.bat
@@ -1,6 +1,5 @@
 @echo off
 rmdir /s /q testing-workspace\workspace
 echo Deleted workspace directory
-xcopy testing-workspace\template testing-workspace\workspace /E /Q
+xcopy testing-workspace\template testing-workspace\workspace /E /Q /C /I /H /Y
 echo Files successfully copied
-pause
