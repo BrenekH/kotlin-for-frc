699074dc2bdf8f17613bce91fab98135d52550d3
MetaHerobrine1 <brenekharrison@gmail.com>
2019-01-25 18:53:14 -0700

    reset script travis testing

--- reset-testing-workspace.sh
+++ reset-testing-workspace.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm -rf testing-workspace/workspace/*
-echo Deleted workspace directory
-cp -a testing-workspace/template/. testing-workspace/workspace/
-echo Files successfully copied
+# rm -rf testing-workspace/workspace/*
+# echo Deleted workspace directory
+# cp -a testing-workspace/template/. testing-workspace/workspace/
+# echo Files successfully copied
+ls
