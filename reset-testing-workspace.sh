e45f2dc0a41b2be7d32fed076513b9942c13bf85
MetaHerobrine1 <brenekharrison@gmail.com>
2019-01-25 19:01:12 -0700

    Changing to actual script

--- reset-testing-workspace.sh
+++ reset-testing-workspace.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# rm -rf testing-workspace/workspace/*
-# echo Deleted workspace directory
-# cp -a testing-workspace/template/. testing-workspace/workspace/
-# echo Files successfully copied
-ls
+rm -rf testing-workspace/workspace/*
+echo Deleted workspace directory
+cp -a testing-workspace/template/. testing-workspace/workspace/
+echo Files successfully copied
