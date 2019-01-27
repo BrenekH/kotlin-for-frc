9d726cff6a719543d52e100696bc4ac39acf5896
MetaHerobrine1 <brenekharrison@gmail.com>
2019-01-26 18:00:04 -0700

    Tests seem to work now

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,7 +3,7 @@ import * as kotlinExt from '../extension';
 // import * as compliance from '../compliance';
 // import * as commands from '../commands';
 // import * as file_generator from '../file_generator';
-// import * as preferences from '../preferences';
+import * as preferences from '../preferences';
 // import * as template_interpreter from '../template_interpreter';
 // import * as path from "path";
 // import * as fs from 'fs';
@@ -18,21 +18,23 @@ import * as assert from 'assert';
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function () {
+    if (typeof vscode.workspace.workspaceFolders === 'undefined') {
+        console.log("Whoops");
+        return;
+    }
+    var uri = vscode.Uri.parse(vscode.workspace.workspaceFolders[0].uri.path.replace("/out/test", "/testing-workspace/workspace"));
+    kotlinExt.setWorkspaceFolderFsPath(uri.fsPath);
+    kotlinExt.setWorkspaceFolderPath(uri.path);
 
     // Defines a Mocha unit test
-    test("Something 1", function() {
-        if (typeof vscode.workspace.workspaceFolders === 'undefined') {
-			console.log("Not a valid workspace");
-            assert.equal(1, 0);
-            return;
-        }
-        var uri = vscode.Uri.parse(vscode.workspace.workspaceFolders[0].uri.path.replace("/out/test", "/testing-workspace/workspace"));
-        kotlinExt.setWorkspaceFolderFsPath(uri.fsPath);
-        kotlinExt.setWorkspaceFolderPath(uri.path);
-
+    test("Setting up workspace folders", function() {
         console.log("Path: " + kotlinExt.getWorkspaceFolderPath());
         console.log("FsPath: " + kotlinExt.getWorkspaceFolderFsPath());
 
         assert.equal(1, 1);
+    });
+
+    test("Preferences Main Kt value", function() {
+        assert.equal(preferences.getMainKt(), false);
     });
 });
