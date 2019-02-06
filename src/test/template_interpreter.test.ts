852e30763e87e9e4a4937bc3406566945c234d9a
MetaHerobrine1 <metaherobrine1@gmail.com>
2019-02-06 15:25:30 -0700

    Added Main.kt Template Object test

--- src/test/template_interpreter.test.ts
+++ src/test/template_interpreter.test.ts
@@ -1,17 +1,20 @@
 // import * as vscode from 'vscode';
-// import * as assert from 'assert';
+import * as assert from 'assert';
 // import * as kotlinExt from '../extension';
 // import * as compliance from '../compliance';
 // import * as commands from '../commands';
 // import * as file_generator from '../file_generator';
 // import * as preferences from '../preferences';
-// import * as template_interpreter from '../template_interpreter';
+import * as template_interpreter from '../template_interpreter';
 // import * as path from "path";
 // import * as fs from 'fs';
 // import * as testingConsts from "./testingConstants";
+import { MainTemplate } from '../templates/frc-kotlin/Main';
 
 suite("Template Grabbing Tests", function () {
-
+    test("Get Main.kt Template Object", function() {
+        assert.equal(template_interpreter.getMainTemplateObject(), new MainTemplate);
+    });
 });
 
 suite("Template Parsing Tests", function() {
