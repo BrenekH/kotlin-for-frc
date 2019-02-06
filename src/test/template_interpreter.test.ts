1dd825aa37936a5069474ae8a65fcb64912c6aeb
MetaHerobrine1 <metaherobrine1@gmail.com>
2019-02-06 15:17:57 -0700

    Added template_interpreter.test.ts

---
+++ src/test/template_interpreter.test.ts
@@ -0,0 +1,19 @@
+// import * as vscode from 'vscode';
+// import * as assert from 'assert';
+// import * as kotlinExt from '../extension';
+// import * as compliance from '../compliance';
+// import * as commands from '../commands';
+// import * as file_generator from '../file_generator';
+// import * as preferences from '../preferences';
+// import * as template_interpreter from '../template_interpreter';
+// import * as path from "path";
+// import * as fs from 'fs';
+// import * as testingConsts from "./testingConstants";
+
+suite("Template Grabbing Tests", function () {
+
+});
+
+suite("Template Parsing Tests", function() {
+    
+});
