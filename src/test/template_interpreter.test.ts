cbcf02dbda46189868b75c4cb57a9e6ae8c00b15
Brenek Harrison <brenekharrison@gmail.com>
2019-02-19 15:28:35 -0700

    Getting the MainTemplate object

--- src/test/template_interpreter.test.ts
+++ src/test/template_interpreter.test.ts
@@ -5,15 +5,16 @@ import * as assert from 'assert';
 // import * as commands from '../commands';
 // import * as file_generator from '../file_generator';
 // import * as preferences from '../preferences';
-// import * as template_interpreter from '../template_interpreter';
+import * as template_interpreter from '../template_interpreter';
 // import * as path from "path";
 // import * as fs from 'fs';
 import * as testingConsts from "./testingConstants";
-// import { MainTemplate } from '../templates/frc-kotlin/Main';
+import { MainTemplate } from '../templates/frc-kotlin/Main';
 
 suite("Template Grabbing Tests", function () {
     test("Get Main.kt Template Object", function() {
-        // assert.equal(template_interpreter.getMainTemplateObject(), new MainTemplate);
+        var thing = new MainTemplate;
+        assert.equal(template_interpreter.getMainTemplateObject().getText(), thing.getText());
         assert.equal(1, 1);
     });
 });
