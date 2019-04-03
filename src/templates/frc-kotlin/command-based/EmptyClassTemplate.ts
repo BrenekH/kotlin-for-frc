73bbf6b6e2648d33d1d6532e75f5739bcccb789f
MetaHerobrine1 <brenekharrison@gmail.com>
2019-04-03 15:31:50 -0600

    Fixed Empty Class Template

--- src/templates/frc-kotlin/command-based/EmptyClassTemplate.ts
+++ src/templates/frc-kotlin/command-based/EmptyClassTemplate.ts
@@ -4,20 +4,20 @@ export class EmptyClassTemplate {
     constructor() {
     this.useAtProjectConversion = false;
     this.text = `/*----------------------------------------------------------------------------*/
-    /* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-    /* Open Source Software - may be modified and shared by FRC teams. The code   */
-    /* must be accompanied by the FIRST BSD license file in the root directory of */
-    /* the project.                                                               */
-    /*----------------------------------------------------------------------------*/
-    
-    package #{PACKAGE}
-    
-    /**
-     * Add your docs here.
-     */
-    class #{NAME} {
-    }
-    `;
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package #{PACKAGE}
+
+/**
+ * Add your docs here.
+ */
+class #{NAME} {
+}
+`;
   }
   
   public getText(): string {
