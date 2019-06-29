8ac40f0ac5e2c4c25aa6c40e5e784fc48d4be348
MetaHerobrine1 <brenekharrison@gmail.com>
2019-06-29 16:41:30 -0600

    Add tools for generating changelog.md.html

--- src/changelog.ts
+++ src/changelog.ts
@@ -37,10 +37,14 @@ function getWebviewContent() {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Cat Coding</title>
+	<title>Kotlin For FRC Changelog</title>
 </head>
 <body>
-	<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+	<h1>Change Log</h1>
+	<h2>1.4.0</h2>
+	<ul>
+		<li>Faster extension loading times using webpack</li>
+	</ul>
 </body>
 </html>`;
 }
