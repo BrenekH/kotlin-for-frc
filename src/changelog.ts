d744c07cd64a73aa98a5862ee5ff11b5899f0f2f
MetaHerobrine1 <brenekharrison@gmail.com>
2019-06-29 16:17:30 -0600

    Basic Webview as Changelog

--- src/changelog.ts
+++ src/changelog.ts
@@ -25,4 +25,22 @@ function extensionWasUpdated(context: vscode.ExtensionContext): boolean {
 	}
 }
 
-function showChangelog() {console.log("Show changelog");}
+export function showChangelog() {
+	const panel = vscode.window.createWebviewPanel('kotlin-for-frcChangelog', 'Kotlin For FRC Changelog', vscode.ViewColumn.One, {});
+
+	panel.webview.html = getWebviewContent();
+}
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cat Coding</title>
+</head>
+<body>
+	<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
+}
