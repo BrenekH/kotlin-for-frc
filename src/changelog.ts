356e01b9b570522d764b1c0dd4b1d62f0025baf5
MetaHerobrine1 <brenekharrison@gmail.com>
2019-06-30 22:54:24 -0600

    Auto-show Changelog toggle

--- src/changelog.ts
+++ src/changelog.ts
@@ -23,6 +23,11 @@ function extensionWasUpdated(context: vscode.ExtensionContext): boolean {
 		return false;
 	}
 
+	// @ts-ignore Note: This shouldn't be needed because true is a default value but it's here anyways
+	if (context.globalState.get("toggleChangelog", true) === false) {
+		return false;
+	}
+
 	if (semver.satisfies(currentVersion, `>${storedVersion}`)) {
 		return true;
 	} else {
