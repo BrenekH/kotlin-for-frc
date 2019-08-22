c7ebac9d927a1769bfd6bd90f9a9e8a73b96d22d
zPaw <brenekharrison@gmail.com>
2019-08-22 15:05:36 -0600

    VSCode sub-1.37.0 protection

--- src/file_manipulation/file_system.ts
+++ src/file_manipulation/file_system.ts
@@ -1,6 +1,7 @@
 'use strict';
 import * as vscode from 'vscode';
 import * as semver from 'semver';
+import * as fs from 'fs';
 
 export var isVscodeFsAvailable = semver.satisfies(vscode.version, ">=1.37.0");
 
@@ -10,15 +11,23 @@ export function mkdir(uri: string) {
 }
 
 export async function readFile(uri: string) {
-	// TODO: Add sub VSCode 1.37.0 protection
-	var contentFromRead = await (vscode.workspace as any).fs.readFile(vscode.Uri.file(uri));
-	var content = Buffer.from(contentFromRead).toString('utf8');
-	return content;	
+	if (isVscodeFsAvailable) {
+		// Only used when VSCode is updated at or past 1.37.0
+		var contentFromRead = await (vscode.workspace as any).fs.readFile(vscode.Uri.file(uri));
+		var content = Buffer.from(contentFromRead).toString('utf8');
+		return content;	
+	} else {
+		return fs.readFileSync(uri, 'utf-8');
+	}
 }
 
 export function writeToFile(uri: string, content: string) {
-	// TODO: Add sub VSCode 1.37.0 protection
-	const writeBytes = Buffer.from(content, 'utf8');
+	if (isVscodeFsAvailable) {
+		// Only used when VSCode is updated at or past 1.37.0
+		const writeBytes = Buffer.from(content, 'utf8');
 
-	(vscode.workspace as any).fs.writeFile(vscode.Uri.file(uri), writeBytes).then(() => {});
+		(vscode.workspace as any).fs.writeFile(vscode.Uri.file(uri), writeBytes).then(() => {});
+	} else {
+		fs.writeFileSync(uri, content);
+	}
 }
