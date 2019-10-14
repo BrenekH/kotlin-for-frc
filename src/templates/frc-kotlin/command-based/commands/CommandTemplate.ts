0dbc8a549fac523107a127f1b430182fb14375b0
zPaw <brenekharrison@gmail.com>
2019-10-13 22:49:26 -0600

    Update CommandTemplate.ts

--- src/templates/frc-kotlin/command-based/commands/CommandTemplate.ts
+++ src/templates/frc-kotlin/command-based/commands/CommandTemplate.ts
@@ -3,7 +3,34 @@ export class CommandTemplate {
 	private text: string;
 	constructor() {
 	  this.useAtProjectConversion = true;
-	  this.text = `
+	  this.text = `/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package #{PACKAGE}
+
+import frc.robot.subsystems.ExampleSubsystem
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+/**
+ * An example command that uses an example subsystem.
+ */
+class #{NAME}(subsystem: ExampleSubsystem) : CommandBase() {
+	val m_subsystem: ExampleSubsystem = subsystem
+
+	/**
+	 * Creates a new #{NAME}.
+	 *
+	 * @param subsystem The subsystem used by this command.
+	 */
+	init {
+		addRequirements(subsystem)
+	}
+}
+
 `;
 	}
 	public getText(): string {
