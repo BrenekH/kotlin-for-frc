43f15dff0332f4b8e113adcda042ff41bf3211ab
Brenek Harrison <41010693+zPaw@users.noreply.github.com>
2020-01-06 22:30:44 -0700

    Release 2020.1.1 to development (#34)
    * Prevent lateinit var not defined when running teleopInit
    
    * 2020.1.1 Release

--- src/util/changelog.ts
+++ src/util/changelog.ts
@@ -47,38 +47,19 @@ function getWebviewContent() {
 	<title>Kotlin For FRC Changelog</title>
 </head>
 <body>
-	<h1>Kotlin For FRC 1.5.0 Newsletter</h1>
-	<p>Hello Kotlin For FRC users!</p>
-	<p>I would first like to thank you all for installing this extension; the amount of downloads is mind-boggling to me.
-		This is the new changelog feature that I’m introducing to Kotlin For FRC.
-		It will automatically show up after Kotlin For FRC has updated so you know what has changed and when, but for the
-		1.5.0 version, I decided to use it as a newsletter to let everyone know where Kotlin For FRC is headed.</p>
-	<p>As with any piece of code, Kotlin For FRC is bound to have bugs and issues lurking throughout the codebase.
-		As such, I would like to invite everyone to both report and fix bugs.
-		The <a href="https://github.com/zPaw/kotlin-for-frc/blob/master/CONTRIBUTING.md">CONTRIBUTING.md</a> file in the
-		repository explains the basics of how to develop Kotlin For FRC for those who want to fix bugs and add features
-		themselves.</p>
-	<p>Speaking of new features, there are many things coming to Kotlin For FRC for the 2020 season, but they need to be
-		tested in a production-like environment.
-		This is why I am asking for volunteers, especially those who have access to the 2020 WPILib Beta program, to test
-		out the new features.
-		While it's not required to beta test Kotlin For FRC, a sign-up form can be found <a href="https://forms.gle/mXPFLX4aSKqaQU5e9">here</a> and at the bottom of this document.
-		If you do not wish to get an email or similar form of communication when a new beta is released you can just
-		download the latest .vsix from the GitHub releases page <a href="https://github.com/zPaw/kotlin-for-frc/releases">here</a>.</p>
-	<p>One controversial feature I have been toying with in the back of my mind is adding anonymized telemetry(anonymous
-		automatic data collection).
-		Some metrics that I believe would be beneficial to track include Visual Studio Code version and amount of active
-		users.
-		More details about the proposed telemetry ideas can be found on GitHub <a href="https://github.com/zPaw/kotlin-for-frc/issues/23">here</a>.
-		In order to get feedback, I have created a survey that includes telemetry questions and other general feedback type
-		questions.
-		The link is <a href="https://forms.gle/s4Xwqubcrz5ybRgs6">here</a> and below.</p>
-	<p>Once again, I thank you for your support and wish for a great build season for everyone!</p>
-	<p>Brenek Harrison</p>
-	<p>Creator of Kotlin For FRC</p>
-	<p>Beta Test Sign Up <a href="https://forms.gle/mXPFLX4aSKqaQU5e9">https://forms.gle/mXPFLX4aSKqaQU5e9</a></p>
-	<p>Telemetry Issue on GitHub <a href="https://github.com/zPaw/kotlin-for-frc/issues/23">https://github.com/zPaw/kotlin-for-frc/issues/23</a></p>
-	<p>Survey <a href="https://forms.gle/s4Xwqubcrz5ybRgs6">https://forms.gle/s4Xwqubcrz5ybRgs6</a></p>
+<h1>Kotlin for FRC Changelog</h1>
+<h2><a href="https://github.com/zPaw/kotlin-for-frc/tree/2020.1.1">2020.1.1</a> (2020-01-06)</h2>
+<p><strong>Implemented enhancements:</strong></p>
+<ul>
+<li>New Command API <a href="https://github.com/zPaw/kotlin-for-frc/issues/19">#19</a></li>
+<li>Use github-changelog-generator for CHANGELOG.md <a href="https://github.com/zPaw/kotlin-for-frc/issues/16">#16</a></li>
+</ul>
+<p><strong>Fixed bugs:</strong></p>
+<ul>
+<li>.kotlin-for-frc appears even when it's not needed <a href="https://github.com/zPaw/kotlin-for-frc/issues/24">#24</a></li>
+</ul>
+<hr>
+<p>Thanks to everyone who downloaded my extension. Here's to a great season!</p>
 </body>
 </html>`;
 }
