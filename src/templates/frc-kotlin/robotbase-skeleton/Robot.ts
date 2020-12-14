ebd87cd0bde8dbe23adbe51f5d332debb466c32d
Brenek Harrison <brenekharrison@gmail.com>
2020-12-13 19:00:42 -0700

    Update RobotBaseSkeleton Template

--- src/templates/frc-kotlin/robotbase-skeleton/Robot.ts
+++ src/templates/frc-kotlin/robotbase-skeleton/Robot.ts
@@ -1,13 +1,6 @@
 export class RobotBaseSkeleton {
-	text: string = `/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+    text: string = `package frc.robot
 
-package frc.robot
-  
 import edu.wpi.first.hal.HAL
 import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj.livewindow.LiveWindow
@@ -19,69 +12,68 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
  * update the build.gradle file in the project.
  */
 class Robot : RobotBase() {
-  @Volatile private var m_exit: Boolean = false;
+    fun robotInit() {}
 
-  fun robotInit() {
-  }
-  
-  fun disabled() {
-  }
+    fun disabled() {}
 
-  fun autonomous() {
-  }
+    fun autonomous() {}
 
-  fun teleop() {
-  }
+    fun teleop() {}
 
-  fun test() {
-  }
+    fun test() {}
 
-  override fun startCompetition() {
-	robotInit()
-	
-	// Tell the DS that the robot is ready to be enabled
-	HAL.observeUserProgramStarting()
+    @Volatile
+    private var exit = false
+    override fun startCompetition() {
+        robotInit()
 
-	while (!Thread.currentThread().isInterrupted() && !m_exit) {
-	  if (isDisabled()) {
-	    m_ds.InDisabled(true)
-	    disabled()
-	    m_ds.InDisabled(false)
-	    while (isDisabled()) {
-	  	  m_ds.waitForData()
-	    }
-	  } else if (isAutonomous()) {
-	    m_ds.InAutonomous(true)
-	    autonomous()
-	    m_ds.InAutonomous(false)
-	    while (isAutonomous() && !isDisabled()) {
-	  	  m_ds.waitForData()
-	    }
-	  } else if (isTest()) {
-	    LiveWindow.setEnabled(true)
-	    Shuffleboard.enableActuatorWidgets()
-	    m_ds.InTest(true)
-	    test()
-	    m_ds.InTest(false)
-	    while (isTest() && isEnabled()) {
-	  	  m_ds.waitForData()
-	    }
-	    LiveWindow.setEnabled(false)
-	    Shuffleboard.disableActuatorWidgets()
-	  } else {
-	    m_ds.InOperatorControl(true)
-	    teleop()
-	    m_ds.InOperatorControl(false)
-	    while (isOperatorControl() && !isDisabled()) {
-	  	  m_ds.waitForData()
-	    }
-	  }
-	}
-  }
+        // Tell the DS that the robot is ready to be enabled
+        HAL.observeUserProgramStarting()
+        while (!Thread.currentThread().isInterrupted && !exit) {
+            when (true) {
+                isDisabled -> {
+                    m_ds.InDisabled(true)
+                    disabled()
+                    m_ds.InDisabled(false)
+                    while (isDisabled) {
+                        m_ds.waitForData()
+                    }
+                }
+                isAutonomous -> {
+                    m_ds.InAutonomous(true)
+                    autonomous()
+                    m_ds.InAutonomous(false)
+                    while (isAutonomousEnabled) {
+                        m_ds.waitForData()
+                    }
+                }
+                isTest -> {
+                    LiveWindow.setEnabled(true)
+                    Shuffleboard.enableActuatorWidgets()
+                    m_ds.InTest(true)
+                    test()
+                    m_ds.InTest(false)
+                    while (isTest && isEnabled) {
+                        m_ds.waitForData()
+                    }
+                    LiveWindow.setEnabled(false)
+                    Shuffleboard.disableActuatorWidgets()
+                }
+                else -> {
+                    m_ds.InOperatorControl(true)
+                    teleop()
+                    m_ds.InOperatorControl(false)
+                    while (isOperatorControlEnabled) {
+                        m_ds.waitForData()
+                    }
+                }
+            }
+        }
+    }
 
-  override fun endCompetition() {
-    m_exit = true
-  }
+    override fun endCompetition() {
+        exit = true
+    }
 }
 `;
 }
