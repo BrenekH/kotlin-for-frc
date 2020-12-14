5bc96c46ce87fe81164e86b3be34935ecd077996
Brenek Harrison <brenekharrison@gmail.com>
2020-12-13 20:32:22 -0700

    Full Command Based templates updated

--- src/templates/frc-kotlin/command-based/subsystems/ProfiledPIDSubsystemTemplate.ts
+++ src/templates/frc-kotlin/command-based/subsystems/ProfiledPIDSubsystemTemplate.ts
@@ -1,40 +1,26 @@
 export class ProfiledPIDSubsystemTemplate {
-  text: string = `/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
-package #{PACKAGE}
+    text: string = `package #{PACKAGE}
 
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem
 
-class #{NAME} : ProfiledPIDSubsystem() {
-  /**
-   * Creates a new #{NAME}.
-   */
-  init {
-    super(
-      // The ProfiledPIDController used by the subsystem
-      ProfiledPIDController(
-        // PID gains
-        0, 0, 0,
+class #{NAME} : ProfiledPIDSubsystem(
+        // The ProfiledPIDController used by the subsystem
+        ProfiledPIDController(0.0, 0.0, 0.0,
+
         // The motion profile constraints
-        TrapezoidProfile.Constraints(0, 0)
-      ))
-  }
+        TrapezoidProfile.Constraints(0.0, 0.0))
+    ) {
 
-  override fun useOutput(output: Double, setpoint: TrapezoidProfile.State) {
-    // Use the output (and optionally the setpoint) here
-  }
+    public override fun useOutput(output: Double, setpoint: TrapezoidProfile.State) {
+        // Use the output (and optionally the setpoint) here
+    }
 
-  override fun getMeasurement(): Double {
-    // Return the process variable measurement here
-    return 0
-  }
+    public override fun getMeasurement(): Double {
+        // Return the process variable measurement here
+        return 0.0
+    }
 }
 `;
 }
