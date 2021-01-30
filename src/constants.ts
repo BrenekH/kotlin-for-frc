a1cc4f9c2c92aca228753e6276bed492afa99029
Brenek Harrison <41010693+BrenekH@users.noreply.github.com>
2021-01-30 00:08:08 -0700

    Update integrated GradleRIO version to 2021.2.2

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const targetGradleRioVersion = "2021.2.1";
+export const targetGradleRioVersion = "2021.2.2";
 export const targetYear = "2021";
 export const simulateCodeTerminalName = "KfF: Simulate Code";
