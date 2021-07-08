21d0839ca8e0ab72837758d2484d7fee62ea45a2
Brenek Harrison <brenekharrison@gmail.com>
2021-07-07 23:44:14 -0600

    simulationTask()

--- src/tasks/provider.ts
+++ src/tasks/provider.ts
@@ -6,13 +6,17 @@ import { getPlatformGradlew, getJavaHomeGradleArg } from "../util/gradle";
 export function getTaskProvider(): vscode.TaskProvider {
 	return {
 		provideTasks(token): Array<vscode.Task> {
-			const execution = new vscode.ShellExecution(`${getPlatformGradlew()} simulateJava ${getJavaHomeGradleArg()}`);
 			return [
-				new vscode.Task({type: "simulateFRCKotlinCode"}, vscode.TaskScope.Workspace, "Simulate FRC Kotlin", "Kotlin-FRC", execution),
+				simulationTask(),
 			];
 		},
 		resolveTask(task: vscode.Task, token: vscode.CancellationToken): vscode.Task {
 			return task;
 		}
 	};
+}
+
+function simulationTask(): vscode.Task {
+	const execution = new vscode.ShellExecution(`${getPlatformGradlew()} simulateJava ${getJavaHomeGradleArg()}`);
+	return new vscode.Task({type: "simulateFRCKotlinCode"}, vscode.TaskScope.Workspace, "Simulate FRC Kotlin", "Kotlin-FRC", execution);
 }
