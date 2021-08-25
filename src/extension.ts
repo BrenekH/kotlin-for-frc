c2799173949f2ed6ad0ad40b795c80004b142a2c
Brenek Harrison <brenekharrison@gmail.com>
2021-08-25 09:45:30 -0600

    TODO statements in activate

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,9 @@
 import * as vscode from "vscode";
 
 export async function activate(context: vscode.ExtensionContext) {
-
+	// TODO: Setup (telemetry, template providers, set isKFFProject variable, is workspace KfF project)
+	// TODO: Startup (GradleRIO updates, check for WPILib extension, display changelog after update, send telemetry startup event)
+	// TODO: Register commands and other on demand stuff
 }
 
 export function deactivate() {}
