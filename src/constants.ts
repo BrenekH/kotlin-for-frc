c1559d707b0d947039116031477ca36c4fd1d3fb
Brenek Harrison <brenekharrison@gmail.com>
2021-08-25 09:21:01 -0600

    Rip out code

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const targetGradleRioVersion = "2021.3.1";
-export const targetYear = "2021";
--- src/tasks/cmdExecution.ts
+++
@@ -1,8 +0,0 @@
-"use strict";
-import * as vscode from "vscode";
-
-export function executeCommand(cmd: string, name: string) {
-	const execution = new vscode.ShellExecution(cmd);
-	const task = new vscode.Task({type: "kffshell"}, vscode.TaskScope.Workspace, name, "Kotlin-FRC", execution);
-	vscode.tasks.executeTask(task);
-}
