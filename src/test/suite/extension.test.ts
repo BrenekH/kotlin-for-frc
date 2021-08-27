11121c9f72bf20f66a50b76fb59d7a07436fb2ef
Brenek Harrison <brenekharrison@gmail.com>
2021-08-26 19:19:28 -0600

    Basic testing

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,9 @@
+import * as assert from 'assert';
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Extension Tests", function () {
+    // Defines a Mocha unit test
+    test("Sample test", function() {
+        assert.strictEqual(1, 1);
+    });
+});
