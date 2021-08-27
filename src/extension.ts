f0075b87064536d1b1b24edaf10d35de8d72773d
Brenek Harrison <brenekharrison@gmail.com>
2021-08-26 19:46:58 -0600

    Super basic setup stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,20 @@
 "use strict";
 import * as vscode from "vscode";
+import { TelemetryReporter } from "./util/telemetry"
 
 export async function activate(context: vscode.ExtensionContext) {
 	// TODO: Setup (telemetry, template providers, set isKFFProject variable, is workspace KfF project)
+	const telemetry = new TelemetryReporter()
+
+	// Read updateGradleRIOVer and showChangelogOnUpdate from vscode settings, taking care to set them to default values in case of an undefined value.
+	let temp: boolean | undefined = vscode.workspace.getConfiguration("kotlinForFRC.gradleRioVersion").get<boolean>("autoUpdate")
+	const updateGradleRIOVer: boolean = temp !== undefined ? temp : true
+	temp = vscode.workspace.getConfiguration("kotlinForFRC.changelog").get<boolean>("showOnUpdate")
+	const showChangelogOnUpdate: boolean = temp !== undefined ? temp : true
+
 	// TODO: Startup (GradleRIO updates, check for WPILib extension, display changelog after update, send telemetry startup event)
+	telemetry.recordActivationEvent(showChangelogOnUpdate, updateGradleRIOVer)
+
 	// TODO: Register commands and other on demand stuff
 }
 
