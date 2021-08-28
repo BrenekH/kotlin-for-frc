ecec93567e283a0965a03c44b65c9e6c753ff65b
Brenek Harrison <brenekharrison@gmail.com>
2021-08-27 21:02:54 -0600

    args are passed test

--- src/test/suite/template/providers.test.ts
+++ src/test/suite/template/providers.test.ts
@@ -20,8 +20,24 @@ suite("TemplateProviderAggregator", function () {
 	})
 
 	test("getTemplate passes arguments to each sub-provider", async function () {
-		// TODO: Create provider object which will always returns null so that each provider is called.
-		// TODO: Add provider as the integrated, user, and workspace providers
+		const integrated = new TestTemplateProvider()
+		const user = new TestTemplateProvider()
+		const workspace = new TestTemplateProvider()
+
+		const aggregator = new TemplateProviderAggregator(integrated, user)
+		aggregator.setWorkspaceProvider(Uri.file("/test/file"), workspace)
+
+		const wrkspceFolder = Uri.file("/test/file")
+		await aggregator.getTemplate(TemplateType.emptyClass, wrkspceFolder)
+
+		assert.strictEqual(integrated.templateTypeArg, TemplateType.emptyClass, "Integrated template provider called with incorrect templateTypeArg.")
+		assert.strictEqual(integrated.workspaceFolderArg, wrkspceFolder, "Integrated template provider called with incorrect workspaceFolderArg.")
+
+		assert.strictEqual(user.templateTypeArg, TemplateType.emptyClass, "User template provider called with incorrect templateTypeArg.")
+		assert.strictEqual(user.workspaceFolderArg, wrkspceFolder, "User template provider called with incorrect workspaceFolderArg.")
+
+		assert.strictEqual(workspace.templateTypeArg, TemplateType.emptyClass, "Workspace template provider called with incorrect templateTypeArg.")
+		assert.strictEqual(workspace.workspaceFolderArg, wrkspceFolder, "Workspace template provider called with incorrect workspaceFolderArg.")
 	})
 
 	test("getTemplate returns early when a provider gives a non-null value", async function () { })
@@ -29,9 +45,13 @@ suite("TemplateProviderAggregator", function () {
 
 class TestTemplateProvider implements ITemplateProvider {
 	getTemplateCalled = false
+	templateTypeArg: TemplateType | undefined = undefined
+	workspaceFolderArg: Uri | undefined = undefined
 
 	async getTemplate(t: TemplateType, workspaceFolder: Uri): Promise<string | null> {
 		this.getTemplateCalled = true
+		this.templateTypeArg = t
+		this.workspaceFolderArg = workspaceFolder
 		return null
 	}
 }
