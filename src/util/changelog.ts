799cbe1f07cc83b562ecc2bbbd6a7b94cb4692d4
Brenek Harrison <brenekharrison@gmail.com>
2021-08-28 15:01:56 -0600

    changelog utilities

---
+++ src/util/changelog.ts
@@ -0,0 +1,59 @@
+import * as vscode from "vscode"
+import * as semver from "semver"
+
+export function displayChangelog(context: vscode.ExtensionContext) {
+    if (extensionWasUpdated(context)) {
+        showChangelog();
+    }
+}
+
+function extensionWasUpdated(context: vscode.ExtensionContext): boolean {
+    const thisExtension = vscode.extensions.getExtension('brenek.kotlin-for-frc');
+    if (thisExtension === undefined) {
+        console.error("thisExtension was undefined, the changelog will not be displayed.");
+        return false;
+    }
+
+    const currentVersion = thisExtension.packageJSON["version"];
+    const storedVersion = context.globalState.get("lastInitVersion", "0.0.0");
+
+    context.globalState.update("lastInitVersion", currentVersion);
+
+    // @ts-ignore Note: This shouldn't be needed because true is a default value but it's here anyways
+    if (vscode.workspace.getConfiguration("kotlinForFRC.changelog").get("showOnUpdate") === false) {
+        return false;
+    }
+
+    return semver.satisfies(currentVersion, `>${storedVersion}`);
+}
+
+export function showChangelog() {
+    const panel = vscode.window.createWebviewPanel('kotlin-for-frcChangelog', 'Kotlin For FRC Changelog', vscode.ViewColumn.One, {})
+
+    panel.webview.html = webviewContent
+}
+
+const webviewContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kotlin For FRC Changelog</title>
+</head>
+<body>
+<h1>Kotlin for FRC Changelog</h1>
+<h2><a href="https://github.com/BrenekH/kotlin-for-frc/tree/2021.5.1">2021.5.1</a> (2021-05-09)</h2>
+<p><strong>Implemented enhancements:</strong></p>
+<ul>
+<li>
+<p>Limited functionality in Untrusted Workspaces</p>
+</li>
+<li>
+<p>Disable KfF in Virtual Workspaces</p>
+</li>
+<li>
+<p>Upgrade <code>lodash</code> to mitigate possible security issue</p>
+</li>
+</ul>
+</body>
+</html>`
