9344b9e39846aefbec890143cff9aa8388ee5c87
Brenek Harrison <brenekharrison@gmail.com>
2021-09-21 16:32:02 -0600

    Add javascript action for release checklist

---
+++ .github/actions/post-release-checklist/src/index.ts
@@ -0,0 +1,49 @@
+import * as core from "@actions/core"
+import * as github from "@actions/github"
+// import { PullRequest } from "@octokit/webhooks-definitions/schema"
+
+async function run(): Promise<void> {
+	try {
+
+		const context = github.context;
+		const octokit = github.getOctokit(core.getInput("github-token"));
+
+		if (context.eventName !== "pull_request") {
+			throw "Not a pull request"
+		}
+
+		if (context.action !== "opened") {
+			throw "Action can only run on Pull Request open events."
+		}
+
+		const pull_request = (context.payload.pull_request as {
+			[key: string]: any;
+			number: number;
+			html_url?: string;
+			body?: string;
+		})
+
+		// Check if merging branch is master
+		if (pull_request["base"].ref !== "master") {
+			return
+		}
+
+		// Check if current branch is of the form (release-*)
+		if (!/release-.*/.test(pull_request["head"].ref)) {
+			return
+		}
+
+		// Comment on the PR
+		octokit.rest.issues.createComment({
+			issue_number: context.issue.number,
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			body: "It looks like you've opened a release PR!\nHere's a handy checklist of things that need to be done before a release.\n\n- [ ] Update RELEASE_NOTES.md\n- [ ] Update CHANGELOG.md\n- [ ] Update Changelog Webview HTML\n- [ ] Update version"
+		})
+
+	} catch (error: any) {
+		core.setFailed(error)
+	}
+}
+
+run()
