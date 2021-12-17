b8fd4b5255e2ef41ba3f18e6bb3402e3f04702d5
Brenek Harrison <brenekharrison@gmail.com>
2021-12-17 14:51:28 -0700

    Use constants for detecting RobotType

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,7 @@ export const TARGET_GRADLE_RIO_VER = "2022.3.1"
 
 export const ROMI_CMD_VARIANT_SEARCH_TERM = `wpi.sim.envVar("HALSIMWS_HOST"`
 export const ROMI_TIMED_VARIANT_SEARCH_TERM = "new RomiDrivetrain()"
+export const CMD_ROBOT_SEARCH_TERM = "edu.wpi.first.wpilibj2.command.Command"
+export const TIMED_ROBOT_USED_SEARCH_TERM = "edu.wpi.first.wpilibj.TimedRobot"
+export const TIMED_ROBOT_SEARCH_TERM = "edu.wpi.first.wpilibj.smartdashboard.SendableChooser"
+export const ROBOT_BASE_SKELE_SEARCH_TERM = "edu.wpi.first.hal.HAL"
