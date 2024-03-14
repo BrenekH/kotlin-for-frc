86392b09561e402c07d5e66fab601e97e6fa0a4f
Brenek Harrison <brenekharrison@gmail.com>
2024-03-14 10:26:16 -0600

    Update GradleRIO version to 2024.3.2

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const SIMULATE_CODE_TASK_NAME = "Simulate FRC Code"
 export const TARGET_GRADLE_RIO_YEAR = "2024"
-export const TARGET_GRADLE_RIO_VER = "2024.3.1"
+export const TARGET_GRADLE_RIO_VER = "2024.3.2"
 
 export const ROMI_CMD_VARIANT_SEARCH_TERM = `wpi.sim.envVar("HALSIMWS_HOST"`
 export const ROMI_TIMED_VARIANT_SEARCH_TERM = "new RomiDrivetrain()"
